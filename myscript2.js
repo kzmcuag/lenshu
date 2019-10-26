6e31da9aeaa81864399d0bb29e25f1e6acbe7081
kzmcuag <57035301+kzmcuag@users.noreply.github.com>
2019-10-26 21:39:49 +0900

    add some files
    Add assets folder, index.html, myscript2.js

---
+++ myscript2.js
@@ -0,0 +1,17 @@
+var song;
+
+function setup() {
+  song = loadSound('assets/CIRCUS_alex.mp3');
+  createCanvas(720, 200);
+  background(255,0,0);
+}
+
+function mousePressed() {
+  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+    song.stop();
+    background(255,0,0);
+  } else {
+    song.play();
+    background(0,255,0);
+  }
+}
